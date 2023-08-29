6d4a20c0c547873bc64bfcf6310c8474fe48ea16
Sagar Tabiyad <sagar.tabiyad@bypeopletechnologies.com>
2023-08-29 10:42:05 +0530

    Update LogisticApplication.java

--- logistic/src/main/java/com/logistic/LogisticApplication.java
+++ logistic/src/main/java/com/logistic/LogisticApplication.java
@@ -3,13 +3,22 @@ package com.logistic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableFeignClients
 public class LogisticApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LogisticApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
 	}
 
 }
